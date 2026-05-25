1546af42b3e467dab15577fda84c5f8f3b2524d4
Piyush Kalambe <piyushkalambe95@gmail.com>
2026-05-25 12:17:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+    try{
+        if(display.value === ""){
+            return;
+        }
+
+        display.value = eval(display.value);
+
+    }catch(error){
+        display.value = "Error";
+
+        setTimeout(() => {
+            display.value = "";
+        }, 1500);
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    const allowedKeys = [
+        "0","1","2","3","4","5","6","7","8","9",
+        "+","-","*","/","%","."
+    ];
+
+    if(allowedKeys.includes(e.key)){
+        appendValue(e.key);
+    }
+    else if(e.key === "Enter"){
+        calculate();
+    }
+    else if(e.key === "Backspace"){
+        deleteLast();
+    }
+    else if(e.key === "Escape"){
+        clearDisplay();
+    }
+});
